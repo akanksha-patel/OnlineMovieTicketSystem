32d917e4ed96bb985bf10d8523b7fdcdec71f4e0
Akanksha patel <Akanksha.patel@bitwiseglobal.com>
2016-09-22 19:39:28 +0530

    more work

--- src/com/bitwise/controllers/MovieDetailServlet.java
+++ src/com/bitwise/controllers/MovieDetailServlet.java
@@ -1,11 +1,18 @@
 package com.bitwise.controllers;
 
 import java.io.IOException;
+import java.util.Iterator;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import javax.websocket.Session;
+
+import com.bitwise.models.Movie;
+import com.bitwise.models.Movies;
 
 @WebServlet("/MovieDetailServlet")
 public class MovieDetailServlet extends HttpServlet {
@@ -19,7 +26,20 @@ public class MovieDetailServlet extends HttpServlet {
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		int movieId = Integer.parseInt(request.getParameter("movieId"));
+		System.out.println(getMovieDetailById(movieId));
+		request.getRequestDispatcher("movieDetails.jsp").forward(request, response);
 	}
-
+	
+	public Movie getMovieDetailById(int movieId){
+		Movies movies = new Movies();
+		for(Movie movie:movies.getMovies()){
+			if(movie.getMovieId()==movieId){
+				return movie;
+			}
+		}
+		return null;
+	}
+	
+	
 }
